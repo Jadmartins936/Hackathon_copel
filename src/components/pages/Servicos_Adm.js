8cdc04c858183343063d09eb51af3e9a04137402
Arisley Cardoso da Silva <148290892+Arisleycds@users.noreply.github.com>
2024-10-20 02:23:58 -0300

    update Forms
    Alteração de nomes

--- src/components/pages/Servicos_Adm.js
+++ src/components/pages/Servicos_Adm.js
@@ -1,12 +1,14 @@
-import Mic from '../img/mic.svg'
+import Mic from "../img/mic.svg";
 
 const MyComponent = () => {
-    return (
-        <div className="Chat">
-        <div className="msg">
-            <img src={Mic} alt="imagem de microfone mutado"/>
-            <p>Olá, em que posso ajudar?</p>
-        </div>
-       </div>
-    )
-} export default MyComponent
+  return (
+    <div className="Chat">
+      <div className="msg">
+        <img src={Mic} alt="imagem de microfone mutado" />
+        <p>Olá, em que posso ajudar?</p>
+      </div>
+    </div>
+  );
+};
+
+export default MyComponent;
