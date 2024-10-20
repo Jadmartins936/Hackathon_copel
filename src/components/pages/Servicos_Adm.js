a81622c3e833684595a4c6b09502bc31509b8314
Arisley Cardoso da Silva <148290892+Arisleycds@users.noreply.github.com>
2024-10-20 09:19:39 -0300

    update

--- src/components/pages/Servicos_Adm.js
+++ src/components/pages/Servicos_Adm.js
@@ -1,15 +1,12 @@
-
-import {ReactComponent as MyLogo} from '../img/mic.svg'
-import '../estilo/Servicos_Adm.css'
+import { ReactComponent as MyLogo } from "../img/mic.svg";
+import "../estilo/Servicos_Adm.css";
 
 const MyComponent = () => {
-    return (
-        <div className="Chat">
-            <MyLogo/>
-            
-            <button className='borda'>Olá, em que posso ajudar? </button></div>
-            
-    )
-} ;
-export default MyComponent
-
+  return (
+    <div className="Chat">
+      <MyLogo />
+      <input className="borda" placeholder="Olá, em que posso ajudar? "></input>
+    </div>
+  );
+};
+export default MyComponent;
