f244f7cfec70ed55a670f634ae80b4d4624f9487
Arisley Cardoso da Silva <148290892+Arisleycds@users.noreply.github.com>
2024-10-20 09:40:28 -0300

    update chat

--- src/components/pages/Servicos_Adm.js
+++ src/components/pages/Servicos_Adm.js
@@ -1,32 +1,27 @@
-import {ReactComponent as More} from '../img/arrow_right_alt_24dp_FF750C_FILL0_wght400_GRAD0_opsz24.svg'
-import {ReactComponent as MyLogo} from '../img/mic.svg'
-import '../estilo/Servicos_Adm.css'
+import { ReactComponent as More } from "../img/arrow_right_alt_24dp_FF750C_FILL0_wght400_GRAD0_opsz24.svg";
+import { ReactComponent as MyLogo } from "../img/mic.svg";
+import "../estilo/Servicos_Adm.css";
 
 const MyComponent = () => {
- 
+  return (
+    <>
+      <div className="Chat">
+        <MyLogo />
+        <button className="borda">Olá, em que posso ajudar? </button>
+      </div>
+      <h1>Serviços</h1>
+      <section id="align">
+        <div className="servicoscard"></div>
+        <div className="servicoscard"></div>
+        <div className="servicoscard"></div>
+        <div className="servicoscard"></div>
+        <div className="servicoscard"></div>
+        <div className="servicoscard"></div>
 
-    return (
-        <>
-        <div className="Chat">
-           
-            <MyLogo/>
-            <button className='borda'>Olá, em que posso ajudar? </button></div>
-            <h1>Serviços</h1> 
-             <section id='align'>
-                
-                <div className='servicoscard'></div>
-                <div className='servicoscard'></div>
-                <div className='servicoscard'></div>
-                <div className='servicoscard'></div>
-                <div className='servicoscard'></div>
-                <div className='servicoscard'></div>
-                
-                <h2>Mais serviços</h2><img src=''></img>
-                </section>
-
-            
-        </>  
-    )
-} ;
-export default MyComponent
-
+        <h2>Mais serviços</h2>
+        <img src=""></img>
+      </section>
+    </>
+  );
+};
+export default MyComponent;
