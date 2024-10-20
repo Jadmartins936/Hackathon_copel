61e78deada5172f0a423a1560514f8ec01f067c5
Jadmartins936 <jadmartins936@gmail.com>
2024-10-20 09:25:04 -0300

    servicos

--- src/components/pages/Servicos_Adm.js
+++ src/components/pages/Servicos_Adm.js
@@ -1,14 +1,31 @@
-
+import {ReactComponent as More} from '../img/arrow_right_alt_24dp_FF750C_FILL0_wght400_GRAD0_opsz24.svg'
 import {ReactComponent as MyLogo} from '../img/mic.svg'
 import '../estilo/Servicos_Adm.css'
 
 const MyComponent = () => {
+ 
+
     return (
+        <>
         <div className="Chat">
+           
             <MyLogo/>
-            
             <button className='borda'>Olá, em que posso ajudar? </button></div>
+            <h1>Serviços</h1> 
+             <section id='align'>
+                
+                <div className='servicoscard'></div>
+                <div className='servicoscard'></div>
+                <div className='servicoscard'></div>
+                <div className='servicoscard'></div>
+                <div className='servicoscard'></div>
+                <div className='servicoscard'></div>
+                
+                <h2>Mais serviços</h2><img src=''></img>
+                </section>
+
             
+        </>  
     )
 } ;
 export default MyComponent
