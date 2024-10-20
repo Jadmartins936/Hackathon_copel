0c1eb1e3afc7bf92765f1e20c1cddbc8de378006
Arisley Cardoso da Silva <148290892+Arisleycds@users.noreply.github.com>
2024-10-20 00:31:50 -0300

    update

--- projeto/src/index.js
+++ projeto/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
